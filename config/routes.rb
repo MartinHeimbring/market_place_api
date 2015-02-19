5c0042de709ee8d004e0099cd446c85cb2a1c103
Martin Heimbring <martin.heimbring@gmail.com>
2015-02-19 16:13:29 +0100

    set route constraints for the API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # Api definition
+  namespace :api , defaults: { format: :json },
+            constraints: { subdomain: 'api' }, path: '/'  do
+            # generate a base_ui under a subdomain
+    # I'll access the api at api.market_place_api.dev (using pow)
 
+  end
 end
