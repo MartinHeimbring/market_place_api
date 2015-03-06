f4fec8de1c8eb11b94f9fd7d1850fba7b08bd314
Martin Heimbring <martin.heimbring@gmail.com>
2015-03-06 09:18:38 +0100

    Adds product model bare bones along with some validations

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Product do
+  let(:product) { FactoryGirl.create :product }
+  subject { product }
+
+  it { should respond_to :published }
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :price }
+  it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+  it { should validate_presence_of :user_id }
+  it { should_not be_published }
+
+
+end
