c25f736ecf40a1b45f781c23cc2950a8fa20e644
Martin Heimbring <martin.heimbring@gmail.com>
2015-03-13 20:22:20 +0100

    Allows the order to be placed along with product quantity

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -54,7 +54,7 @@ describe Api::V1::OrdersController do
 
       product_1 = FactoryGirl.create :product
       product_2 = FactoryGirl.create :product
-      order_params = { total: 50, user_id: current_user.id, product_ids: [product_1.id, product_2.id] }
+      order_params = { product_ids_and_quantities: [[product_1.id, 2],[ product_2.id, 3]] }
       post :create, user_id: current_user.id, order: order_params
     end
 
@@ -66,18 +66,6 @@ describe Api::V1::OrdersController do
     it { should respond_with 201 }
   end
 
-  describe "#set_total!" do
-    before(:each) do
-      product_1 = FactoryGirl.create :product, price: 100
-      product_2 = FactoryGirl.create :product, price: 85
-
-      @order = FactoryGirl.build :order, product_ids: [product_1.id, product_2.id]
-    end
-
-    it "returns the total amount to pay for the products" do
-      expect{@order.set_total!}.to change{@order.total}.from(0).to(185)
-    end
-  end
 
 end
 
